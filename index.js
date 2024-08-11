e91e50fcdbc1b30c919edd24f50ea6d700575342
AtiqurRahman895 <emonhassan895@gmail.com>
2024-08-12 00:36:02 +0600

    ADDED HERO SECTION

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+window.addEventListener(`scroll`,()=>{
+        if(window.scrollY>=20){
+                document.querySelector(`.headerSection`).classList.add(`headerSectionAnimation`)
+        }else{
+                document.querySelector(`.headerSection`).classList.remove(`headerSectionAnimation`)
+        }
+})
+
+
 const menusToggler=document.querySelector(`.menusToggler`)
 const toggleTarget=document.querySelector(`.toggleTarget`)
 menusToggler.addEventListener(`click`,(e)=>{
